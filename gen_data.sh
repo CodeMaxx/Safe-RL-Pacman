b3e2bb1e60ade09794e0e72409ad0e405759f30a
Samar97 <samar19972102@gmail.com>
2018-11-23 23:24:26 +0530

    Script changed for intelligent ghosts

--- gen_data.sh
+++ gen_data.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 declare -a algorithm_list=("ApproximateQAgent" "PacmanQAgent")
-declare -a horizon_list=(1000 2000)
-# declare -a instance_grid_list=("smallClassic")
-declare -a instance_grid_list=("smallGrid" "mediumGrid")
+declare -a horizon_list=(2000)
+declare -a instance_grid_list=("smallGrid")
 
 for algorithm in "${algorithm_list[@]}"
 do
@@ -14,8 +13,8 @@ do
 		for instance in "${instance_grid_list[@]}"
 		do
 			echo "		$instance"
-			python pacman.py -p $algorithm -x $horizon -n $horizon -l $instance -s > data/$algorithm-$instance-shield-$horizon.dat
-			python pacman.py -p $algorithm -x $horizon -n $horizon -l $instance > data/$algorithm-$instance-normal-$horizon.dat
+			python pacman.py -p $algorithm -x $horizon -n $horizon -l $instance -s -g DirectionalGhost > data/$algorithm-$instance-shield-intelligent-$horizon.dat
+			python pacman.py -p $algorithm -x $horizon -n $horizon -l $instance -g DirectionalGhost > data/$algorithm-$instance-normal-intelligent-$horizon.dat
 		done
 	done
 done
